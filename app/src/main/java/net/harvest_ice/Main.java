166c44c6a28110c942dc4e17d7111a9ee6017980
Dukantic <aubindo@outlook.fr>
2025-01-29 12:43:54 +0100

    add timebreak, money

--- app/src/main/java/net/harvest_ice/Main.java
+++ app/src/main/java/net/harvest_ice/Main.java
@@ -4,9 +4,9 @@ import net.harvest_ice.utils.*;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Start Game!");
         click variClick = new click();
-        variClick.wasClicked();
+        variClick.tick();
         System.out.println(variClick); 
     }
 }
--- app/src/main/java/net/harvest_ice/utils/click.java
+++ app/src/main/java/net/harvest_ice/utils/click.java
@@ -2,21 +2,33 @@
 package net.harvest_ice.utils;
 
 public class click {
-    float count = 0;
+    private float money = 0;
+    private float count = 0;
+    private float timeBreak = 1;
+    private float moneyPerBreak = 5;
 
     public click()
     {
+        this.money = 0;
         this.count = 0;
+        this.timeBreak = 1;
+        this.moneyPerBreak = 5;
     }
 
-    public void wasClicked()
+    public void tick()
     {
-        this.count++;
+        this.count+= 2;
+
+        if (this.count >= this.timeBreak)
+        {
+            this.count -= this.timeBreak;
+            this.money += moneyPerBreak;
+        }
     }
 
     @Override
     public String toString()
     {
-        return String.valueOf(count);
+        return String.valueOf(this.money);
     }
 }
