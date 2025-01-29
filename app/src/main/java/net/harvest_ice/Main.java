f8c21f9d58dec64e3623b86c68509741822bc359
Dukantic <aubindo@outlook.fr>
2025-01-29 13:15:02 +0100

    change delay to time

--- app/src/main/java/net/harvest_ice/Main.java
+++ app/src/main/java/net/harvest_ice/Main.java
@@ -1,12 +1,21 @@
 package  net.harvest_ice;
 
 import net.harvest_ice.utils.*;
+import java.time.*;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Start Game!");
         click variClick = new click();
-        variClick.tick();
         System.out.println(variClick); 
+        boolean loop = true;
+        while(loop)
+        {
+            variClick.tick();
+            if(variClick.getMoney() >= 20)
+            {
+                loop = false;
+            }
+        }
     }
 }
--- app/src/main/java/net/harvest_ice/utils/click.java
+++ app/src/main/java/net/harvest_ice/utils/click.java
@@ -1,34 +1,45 @@
 
 package net.harvest_ice.utils;
+import java.time.*;
 
 public class click {
     private float money = 0;
-    private float count = 0;
-    private float timeBreak = 1;
+    private long timeBreak;
     private float moneyPerBreak = 5;
+    LocalTime nextBreak ;
 
     public click()
     {
         this.money = 0;
-        this.count = 0;
         this.timeBreak = 1;
         this.moneyPerBreak = 5;
+        this.nextBreak = LocalTime.now().plusSeconds(timeBreak);
+    }
+
+    public float getMoney()
+    {
+        return this.money;
+    }
+
+    private void breakIce()
+    {
+        if (LocalTime.now().isAfter(this.nextBreak))
+        {
+            this.nextBreak = LocalTime.now().plusSeconds(this.timeBreak);
+            this.money += moneyPerBreak;
+            System.out.println(this);
+        }
     }
 
     public void tick()
     {
-        this.count+= 2;
-
-        if (this.count >= this.timeBreak)
-        {
-            this.count -= this.timeBreak;
-            this.money += moneyPerBreak;
-        }
+        
+        this.breakIce();
     }
 
     @Override
     public String toString()
     {
-        return String.valueOf(this.money);
+        return String.valueOf(this.money) + "¤";
     }
 }
